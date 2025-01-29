da8c7b8ca714edf796384f26002252bfebf99b6b
Soucius <soucius.ggwp@gmail.com>
2025-01-29 17:29:57 +0300

    first commit

---
+++ API/Data/DataContext.cs
@@ -0,0 +1,8 @@
+using API.Entity;
+using Microsoft.EntityFrameworkCore;
+
+namespace API.Data;
+
+public class DataContext(DbContextOptions options): DbContext(options) {
+    public DbSet<Product> Products => Set<Product>();
+}
